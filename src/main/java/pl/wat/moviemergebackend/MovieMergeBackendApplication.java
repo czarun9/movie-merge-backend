d8ebd3ee6f9cafea0830e77fe6eb9e18c4eca55a
Cezary <czarun99@gmail.com>
2024-06-01 12:46:34 +0200

    init commit

---
+++ src/main/java/pl/wat/moviemergebackend/MovieMergeBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.wat.moviemergebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMergeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieMergeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wat/moviemergebackend/MovieMergeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wat.moviemergebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMergeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
