321594999451f96757bb4f1736a90a3671c50ead
Cezary <czarun99@gmail.com>
2025-05-04 22:12:00 +0200

    dodanie obsługi LocalDateTime

--- src/main/java/pl/wat/moviemergebackend/config/ObjectMapperConfig.java
+++ src/main/java/pl/wat/moviemergebackend/config/ObjectMapperConfig.java
@@ -3,6 +3,7 @@ package pl.wat.moviemergebackend.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,6 +15,7 @@ public class ObjectMapperConfig {
     @Primary
     public ObjectMapper createObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
